6f93d55871059ef3dab850755f38252ef6f88b74
Bruno <brunoolijuca@gmail.com>
2020-10-27 12:52:12 -0300

    Updates

---
+++ precache-manifest.547b49855dd974ea8dd23dcc8ac8ed0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44212e80e9872bff2a9082cc62407814",
+    "url": "/covid-app/index.html"
+  },
+  {
+    "revision": "dc937de17381c5e1f7b5",
+    "url": "/covid-app/static/css/main.658b0913.chunk.css"
+  },
+  {
+    "revision": "58758fb55686fe9ec7c2",
+    "url": "/covid-app/static/js/2.8728d5c8.chunk.js"
+  },
+  {
+    "revision": "639c28c8ef14bcd37d017fe8f6dc04e2",
+    "url": "/covid-app/static/js/2.8728d5c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc937de17381c5e1f7b5",
+    "url": "/covid-app/static/js/main.b8e52e7d.chunk.js"
+  },
+  {
+    "revision": "60a811fca23cbdf231cf",
+    "url": "/covid-app/static/js/runtime-main.61ce12e1.js"
+  }
+]);
